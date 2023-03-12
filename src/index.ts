685bf69078c1e44125d2daa7f2d3c8255e2e76ff
Umberto Loria <umberto.loria@gmail.com>
2023-03-12 12:56:10 +0100

    Improved lib, renamed first image, added second image

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,71 @@
 import jimp from 'jimp';
 import * as fs from 'fs';
-import {add, addList, getG, getInterval, mult, onlyBlue, onlyGreen, onlyRed, RGB, safeColor, scalar, sub} from './lib';
+import {
+    add,
+    addList,
+    color,
+    filterColor,
+    filterScalar,
+    getG,
+    getR,
+    mult,
+    onlyB,
+    onlyG,
+    onlyR,
+    RGB,
+    safeColor,
+    scalar,
+    sub
+} from './lib';
 
-const filesFilters: { [key: string]: (rgba: RGB) => void | undefined } = {
+const filesFilters: { [key: string]: ((rgba: RGB) => RGB) | undefined } = {
 
-    '1.jpg': (rgba: RGB) => {
-        let color: RGB = {
-            r: rgba.r / 255.0,
-            g: rgba.g / 255.0,
-            b: rgba.b / 255.0,
-        };
-
+    '20230226_201501.jpg'(c) {
         const CUSTOM_FILTER_1 = (c: RGB) => safeColor(((1 - c.b) - .5) * 10 + .5)
-        const custom_channel_1 = CUSTOM_FILTER_1(color);
+        const custom_channel_1 = CUSTOM_FILTER_1(c);
 
-        color = addList(
+        return addList(
             mult(
                 add(
-                    onlyGreen(custom_channel_1 * .2),
-                    onlyBlue(custom_channel_1),
+                    onlyG(custom_channel_1 * .2),
+                    onlyB(custom_channel_1),
                 ),
                 scalar(.85)
             ),
-            onlyRed(
+            onlyR(
                 getG(
                     sub(
-                        getInterval(color, .5, .7),
+                        filterColor(c, .5, .7),
                         scalar(.5),
                     ),
                 ),
             ),
         );
+    },
 
-        rgba.r = safeColor(color.r * 255.0);
-        rgba.g = safeColor(color.g * 255.0);
-        rgba.b = safeColor(color.b * 255.0);
+    '20230301_224920.jpg'(c) {
+        return addList(
+            mult(
+                scalar(filterScalar(getR(c), 0.6, 0.85) - 0.6),
+                color(
+                    10,
+                    5,
+                    0,
+                ),
+            ),
+            onlyB(.35),
+        );
+        /*return xorColor(
+            mult(
+                scalar(.4 - filterScalar(getR(c), 0.35, 0.4)),
+                color(
+                    7,
+                    10,
+                    5,
+                ),
+            ),
+            onlyB(.35),
+        );*/
     },
 
 };
@@ -42,16 +73,24 @@ const filesFilters: { [key: string]: (rgba: RGB) => void | undefined } = {
 (async () => {
     const files = fs.readdirSync('input');
     for (const file of files) {
-        const fileFilter = filesFilters["1.jpg"];
+        const fileFilter = filesFilters[file];
         if (!!fileFilter) {
             const image = await jimp.read(`./input/${file}`);
+            // For resize large images.
+            // image.resize(1000, 1000 / (image.getWidth() / image.getHeight()));
             const width = image.getWidth();
             const height = image.getHeight();
             for (let x = 0; x < width; ++x) {
                 for (let y = 0; y < height; ++y) {
-                    const colorHex = image.getPixelColor(x, y);
-                    const rgba = jimp.intToRGBA(colorHex);
-                    fileFilter(rgba);
+                    const rgba = jimp.intToRGBA(image.getPixelColor(x, y));
+                    const color = fileFilter({
+                        r: rgba.r / 255.0,
+                        g: rgba.g / 255.0,
+                        b: rgba.b / 255.0,
+                    });
+                    rgba.r = safeColor(color.r * 255.0);
+                    rgba.g = safeColor(color.g * 255.0);
+                    rgba.b = safeColor(color.b * 255.0);
                     image.setPixelColor(
                         jimp.rgbaToInt(rgba.r, rgba.g, rgba.b, rgba.a),
                         x,
--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,3 @@
-type Filter = (c: RGB) => RGB
 export type RGB = {
     r: number;
     g: number;
@@ -6,7 +5,6 @@ export type RGB = {
 }
 
 export const safeColor = (color: number) => filterScalar(color, 0, 255);
-export const wrap = (filter: Filter, c: RGB) => copy(filter(copy(c)));
 export const copy = (c: RGB): RGB => ({
     r: c.r,
     g: c.g,
@@ -21,17 +19,29 @@ export const filterScalar = (scalar: number, min: number, max: number) => (
                 : scalar
         )
 );
-export const getInterval = (c: RGB, min: number, max: number): RGB => ({
+export const filterColor = (c: RGB, min: number, max: number): RGB => ({
     r: filterScalar(c.r, min, max),
     g: filterScalar(c.g, min, max),
     b: filterScalar(c.b, min, max),
 });
+export const filterScalarAndStretch = (scalar: number, min: number, max: number) => (filterScalar(scalar, min, max) - min) * (1 / (max - min))
+export const filterColorAndStretch = (c: RGB, min: number, max: number) => addList(
+    onlyR(filterScalarAndStretch(getR(c), min, max)),
+    onlyG(filterScalarAndStretch(getG(c), min, max)),
+    onlyB(filterScalarAndStretch(getB(c), min, max)),
+)
+
 export const zero = (): RGB => ({
     r: 0,
     g: 0,
     b: 0,
 });
 export const one = () => scalar(1);
+export const color = (r: number, g: number, b: number): RGB => ({
+    r,
+    g,
+    b,
+});
 export const add = (a: RGB, b: RGB): RGB => ({
     r: a.r + b.r,
     g: a.g + b.g,
@@ -53,17 +63,17 @@ export const scalar = (scalar: number): RGB => ({
     g: scalar,
     b: scalar,
 });
-export const onlyRed = (r: number): RGB => ({
+export const onlyR = (r: number): RGB => ({
     r,
     g: 0,
     b: 0,
 });
-export const onlyGreen = (g: number): RGB => ({
+export const onlyG = (g: number): RGB => ({
     r: 0,
     g,
     b: 0,
 });
-export const onlyBlue = (b: number): RGB => ({
+export const onlyB = (b: number): RGB => ({
     r: 0,
     g: 0,
     b,
@@ -86,3 +96,19 @@ export const addList = (...cList: RGB[]): RGB => {
         b,
     };
 };
+
+export const xorColor = (a: RGB, b: RGB) => color(
+    Math.abs(a.r - b.r),
+    Math.abs(a.g - b.g),
+    Math.abs(a.b - b.b),
+)
+
+export const mean = (c: RGB): number => (c.r + c.g + c.b) / 3.0;
+
+export const blend = (a: RGB, b: RGB, percentage: number) => add(
+    mult(a, scalar(percentage)),
+    mult(b, scalar(1 - percentage)),
+);
+
+// type Filter = (c: RGB) => RGB
+// export const wrap = (filter: Filter, c: RGB) => copy(filter(copy(c)));
